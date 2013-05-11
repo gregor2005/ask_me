6eb2b8559b4b44726207733c3a8a0fff9b9faedc
gregor <gregor.binder@gmx.net>
2013-05-11 14:50:14 +0200

    added block answer feature

--- src/ask_me.rb
+++ src/ask_me.rb
@@ -15,16 +15,48 @@ if !FileTest.file?(questions_file)
 end
 
 question = ""
+block_found_start = false
+block_found_end = false
+answer = []
 
 File.open(questions_file, "r").each do |line|
+  puts line
   if line.match(/^q:(.*)/)
     question = $1
+    puts "question found"
   end
   if line.match(/^a:(.*)/)
-    if (question.size > 0)
-      question_answer.store(question, $1);
+    temp_answer = $1
+    if $1.match(/.*#{answer_block_start}(.*)/)
+      puts "start found"
+      block_found_start = true
+      answer << $1
     end
-    question = ""
+    if (question.size > 0)
+      if (!block_found_start)
+        puts "store single answer"
+        question_answer.store(question, temp_answer);
+        question = ""
+      end
+    end
+  else
+    if line.match(/(.*)#{answer_block_end}/)
+      puts "end found"
+      block_found_end = true
+      answer << $1
+    end
+    if block_found_start && !block_found_end
+      puts "store block line"
+      answer << line
+    end
+    if (block_found_start && block_found_end)
+      puts "store block"
+      question_answer.store(question, answer);
+      question = ""
+      block_found_start = false
+      block_found_end = false
+      answer = []
+    end
   end
 end
 
@@ -35,6 +67,12 @@ question_answer.each do |key,value|
   puts "#{key}"
   puts "----------------"
   puts "--- answer ---"
-  puts "#{value}"
+  if value.kind_of?(Array)
+    value.each do |line|
+      puts line
+    end
+  else
+    puts "#{value}"
+  end
   puts "--------------"
 end
